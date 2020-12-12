9ae1e93b0230288cbaab44580cb62cb5ee818695
Vladimir <riuson@gmail.com>
2020-12-13 01:22:35 +0500

    Cleanup

--- Leopold95/Parser/Parser.cs
+++ Leopold95/Parser/Parser.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Windows.Controls;
 using System.Windows.Documents;
 
 namespace Leopold95.Parser
